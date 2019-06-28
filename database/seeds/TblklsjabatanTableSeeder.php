911e31ac892d734b1ad3ebd2b915dd552b69a989
Dimas Harsono <dmh2907@gmail.com>
2019-06-28 09:31:08 +0700

    seed for HR | dimas

--- database/seeds/TblklsjabatanTableSeeder.php
+++ database/seeds/TblklsjabatanTableSeeder.php
@@ -20,11 +20,155 @@ class TblklsjabatanTableSeeder extends Seeder
             0 => 
             array (
                 'id' => 1,
-                'nama_kj' => 'Kelas Jabatan A',
-                'jml_butuh' => 1,
+                'nama_kj' => '16',
+                'jml_butuh' => 0,
                 'deleted_at' => NULL,
-                'created_at' => '2019-06-01 05:20:10',
-                'updated_at' => '2019-06-01 05:20:10',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'nama_kj' => '10',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'nama_kj' => '13',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'nama_kj' => '12',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'nama_kj' => '11',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'nama_kj' => '14',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'nama_kj' => '9',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'nama_kj' => '8',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            8 => 
+            array (
+                'id' => 9,
+                'nama_kj' => '7',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            9 => 
+            array (
+                'id' => 10,
+                'nama_kj' => '15',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            10 => 
+            array (
+                'id' => 11,
+                'nama_kj' => 'PKWT',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            11 => 
+            array (
+                'id' => 12,
+                'nama_kj' => '18',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            12 => 
+            array (
+                'id' => 13,
+                'nama_kj' => '19',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            13 => 
+            array (
+                'id' => 14,
+                'nama_kj' => '6',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            14 => 
+            array (
+                'id' => 15,
+                'nama_kj' => 'KMPG',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            15 => 
+            array (
+                'id' => 16,
+                'nama_kj' => '17',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            16 => 
+            array (
+                'id' => 17,
+                'nama_kj' => '21',
+                'jml_butuh' => 0,
+                'deleted_at' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
             ),
         ));
         
