3d54a2ab25c771e07197c9ee0ce1611e9a600b54
Dimas Harsono <dmh2907@gmail.com>
2019-06-28 09:13:48 +0700

    dimas push, 28-6-19

--- resources/views/fungsis/show_fields.blade.php
+++ resources/views/fungsis/show_fields.blade.php
@@ -1,36 +1,41 @@
-<!-- id Field -->
-<div class="form-group">
-    {!! Form::label('id', 'id:') !!}
-    <p>{!! $fungsi->id !!}</p>
+<div class="row">
+  <div class="col-md-3"><!-- id Field -->
+    <div class="form-group">
+        {!! Form::label('id', 'id:') !!}
+        <p>{!! $fungsi->id !!}</p>
+    </div>
+  </div>
+  <div class="col-md-3">
+    <div class="form-group">
+        {!! Form::label('nama_fungsi', 'Nama Fungsi:') !!}
+        <p>{!! $fungsi->nama_fungsi !!}</p>
+    </div>
+  </div>
+<div class="col-md-3">
+    <div class="form-group">
+        {!! Form::label('jml_butuh', 'Jml Butuh:') !!}
+        <p>{!! $fungsi->jml_butuh !!}</p>
+    </div>
+  </div>
+  <div class="col-md-3">
+    <div class="form-group">
+        {!! Form::label('deleted_at', 'Deleted At:') !!}
+        <p>{!! $fungsi->deleted_at !!}</p>
+    </div>
+  </div>
 </div>
 
-<!-- Nama Fungsi Field -->
-<div class="form-group">
-    {!! Form::label('nama_fungsi', 'Nama Fungsi:') !!}
-    <p>{!! $fungsi->nama_fungsi !!}</p>
+<div class="row">
+    <div class="col-md-6">
+    <div class="form-group">
+        {!! Form::label('created_at', 'Created At:') !!}
+        <p>{!! $fungsi->created_at !!}</p>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="form-group">
+        {!! Form::label('updated_at', 'Updated At:') !!}
+        <p>{!! $fungsi->updated_at !!}</p>
+    </div>
+  </div>
 </div>
-
-<!-- Jml Butuh Field -->
-<div class="form-group">
-    {!! Form::label('jml_butuh', 'Jml Butuh:') !!}
-    <p>{!! $fungsi->jml_butuh !!}</p>
-</div>
-
-<!-- Deleted At Field -->
-<div class="form-group">
-    {!! Form::label('deleted_at', 'Deleted At:') !!}
-    <p>{!! $fungsi->deleted_at !!}</p>
-</div>
-
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{!! $fungsi->created_at !!}</p>
-</div>
-
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p>{!! $fungsi->updated_at !!}</p>
-</div>
-
