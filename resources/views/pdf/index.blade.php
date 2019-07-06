770528e2da52fb04f7c4613acfd00c986cb914b3
CalvinJO <cjob280@gmail.com>
2019-07-06 14:45:28 +0700

    ix pdf formasi and style pdf

--- resources/views/pdf/index.blade.php
+++ resources/views/pdf/index.blade.php
@@ -18,6 +18,53 @@ th{
     border : 1px solid black;
     padding : 4px;
 }
+*{
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+}
+table.paleBlueRows {
+  border: 1px solid #FFFFFF;
+  width: 350px;
+  height: 200px;
+  text-align: center;
+  border-collapse: collapse;
+}
+table.paleBlueRows td, table.paleBlueRows th {
+    border : 1px solid #424242;
+  padding: 3px 2px;
+}
+table.paleBlueRows tbody td {
+  font-size: 13px;
+  border : 1px solid #424242;
+}
+table.paleBlueRows tr:nth-child(even) {
+  background: #D0E4F5;
+}
+table.paleBlueRows thead {
+  background: #0B6FA4;
+  border-bottom: 5px solid #FFFFFF;
+}
+table.paleBlueRows thead th {
+  font-size: 17px;
+  font-weight: bold;
+  color: #FFFFFF;
+  text-align: center;
+  border-left: 2px solid #FFFFFF;
+}
+table.paleBlueRows thead th:first-child {
+  border-left: none;
+}
+
+table.paleBlueRows tfoot {
+  font-size: 14px;
+  font-weight: bold;
+  color: #333333;
+  background: #D0E4F5;
+  border-top: 3px solid #444444;
+}
+table.paleBlueRows tfoot td {
+  font-size: 14px;
+}
 </style>
 <head>
 	<title>PDF</title>
@@ -32,7 +79,7 @@ th{
         </div>
         <div class="col-sm-12">
             <div class="table">
-                <table class="table table-condensed" style="width:100%">
+                <table class="paleBlueRows" style="width:100%">
                     <tr>
                         <th>No</th>
                         @foreach($head as $dt)
@@ -47,7 +94,7 @@ th{
                             <td><center>{{$key+1}}</center></td>
                                 @foreach($dt as $key2 => $dt2)
                                     <td>
-                                        {{$dt2}}
+                                        {!! $dt2 !!}
                                     </td>
                                 @endforeach
                             </tr>
