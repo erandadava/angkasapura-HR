9360b5932a005f1d0b6d0063f593c32b93f8f274
erandadava <erandadava@gmail.com>
2019-08-21 23:19:37 +0700

    update Add New OS Performance pada Vendor

--- resources/views/osperformances/index.blade.php
+++ resources/views/osperformances/index.blade.php
@@ -4,10 +4,16 @@
     <section class="content-header">
         <h1 class="pull-left">OS Performance</h1>
         <h1 class="pull-right">
+            @hasrole('Vendor')
             <div class="btn-group">
                 <a class="btn btn-warning" style="margin-top: -10px;margin-bottom: 5px" href="/exportpdf/{{Crypt::encrypt('osperformance')}}" target="_blank" >Export To PDF</a>
-                <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('osperformances.create') !!}">Add New</a>
+                {{-- <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('osperformances.create') !!}">Add New</a> --}}
             </div>
+            @endhasrole
+            @hasrole('Super Admin|Admin')
+                <a class="btn btn-warning" style="margin-top: -10px;margin-bottom: 5px" href="/exportpdf/{{Crypt::encrypt('osperformance')}}" target="_blank" >Export To PDF</a>
+                <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('osperformances.create') !!}">Add New</a>
+            @endhasrole
         </h1>
     </section>
     <div class="content">
